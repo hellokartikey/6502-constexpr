6c11a90c5e38e1d136b17cee6c7dfbcfc88bb69d
Kartikey Subramanium <kartikey@tutanota.com>
2024-08-21 19:42:11 +0530

    Boilerplate

---
+++ src/core.h
@@ -0,0 +1,4 @@
+#ifndef CONSTEXPR_6502_CORE_H
+#define CONSTEXPR_6502_CORE_H
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <fmt/base.h>
+
+int main() {
+  fmt::print("Hello, world!!! :)\n");
+
+  return 0;
+}
