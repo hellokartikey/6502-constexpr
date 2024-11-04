1838cac7908efb1ba347f164d41731b3e999eb0d
Aditya Dutt <duttaditya18@gmail.com>
2024-11-04 16:57:00 +0530

    Fixed asm2arr.py to not copy newline and usage

--- scripts/asm2arr.py
+++ scripts/asm2arr.py
@@ -4,7 +4,7 @@ import os
 from sys import argv
 
 if len(argv) != 2 or not argv[1].endswith('a65'):
-    print('Usage: {argv[1]} [filename.a65]')
+    print(f'Usage: {argv[1]} [filename.a65]')
     exit(1)
 
 out = argv[1].replace('.a65', '.o65')
@@ -18,10 +18,10 @@ else:
         a = '{' + ', '.join(f'0x{b:02x}' for b in bs) + '}'
         print(a)
         code = 0
-        if (os.system('which xclip')): # if xclip not present
-            code = os.system(f'echo {a} | wl-copy')
+        if (os.system('which xclip &> /dev/null')): # if xclip not present
+            code = os.system(f'echo -n {a} | wl-copy')
         else:
-            code = os.system(f'echo {a} | xclip -sel c')
+            code = os.system(f'echo -n {a} | xclip -sel c')
         if code == 0:
             print('Output copied to clipboard')
         else:
